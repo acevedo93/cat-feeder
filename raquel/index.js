56ab6b717a7859b5be3e5ed4646ec6c40d255735
acevedo9303 <david.acevedo@vascarsolutions.com>
2022-05-12 10:48:40 -0500

    basic routes to backend

--- raquel/index.js
+++ raquel/index.js
@@ -1,1 +1,18 @@
-console.log('hola esto es un archivo de prueba ');
+const express = require('express');
+const app = express();
+
+app.get('/schedule', (req, res) => {
+  res.json({ data: ' this is the schedule' });
+});
+
+app.post('/schedule', (req, res) => {
+  res.json({ data: 'new schedule created' });
+});
+
+app.post('/feed', (req, res) => {
+  res.json({ data: 'cat feed' });
+});
+
+app.listen(3000, () => {
+  console.log('listen');
+});
